bbbf10f8eb3e4ba75ed8086cf1b50d39a2944609
Alessandro Arosio <alessandro.arosio1@gmail.com>
2021-11-06 18:24:38 +0000

    adding few components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,25 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {HeaderComponent} from './components/header/header.component';
+import {RestaurantsComponent} from './components/restaurants/restaurants.component';
+import {ThingsToDoComponent} from './components/things-to-do/things-to-do.component';
+import {TransportComponent} from './components/transport/transport.component';
+import {BookingInfoComponent} from './components/booking/booking-info/booking-info.component';
+import {BookingListComponent} from './components/booking/booking-list/booking-list.component';
+import {BookingNewComponent} from './components/booking/booking-new/booking-new.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RestaurantsComponent,
+    ThingsToDoComponent,
+    TransportComponent,
+    BookingInfoComponent,
+    BookingListComponent,
+    BookingNewComponent
   ],
   imports: [
     BrowserModule
